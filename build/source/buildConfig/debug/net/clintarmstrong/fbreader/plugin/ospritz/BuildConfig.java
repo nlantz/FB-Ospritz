84612daf8742019f94d298b005ff701a6a939ef8
clint <github@clintarmstrong.net>
2014-03-14 11:01:32 -0400

    Signed Release

--- build/source/buildConfig/debug/net/clintarmstrong/fbreader/plugin/ospritz/BuildConfig.java
+++ build/source/buildConfig/debug/net/clintarmstrong/fbreader/plugin/ospritz/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String PACKAGE_NAME = "net.clintarmstrong.fbreader.plugin.ospritz";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = -1;
+  public static final int VERSION_CODE = 1;
 }
