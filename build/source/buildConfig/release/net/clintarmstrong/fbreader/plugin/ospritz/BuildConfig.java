3cba5b48493b42cf5cc00021029c192bb6eb45a9
clint <github@clintarmstrong.net>
2014-03-17 17:02:06 -0400

    Added light mode - ICS+ Only

--- build/source/buildConfig/release/net/clintarmstrong/fbreader/plugin/ospritz/BuildConfig.java
+++ build/source/buildConfig/release/net/clintarmstrong/fbreader/plugin/ospritz/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String PACKAGE_NAME = "net.clintarmstrong.fbreader.plugin.ospritz";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 6;
+  public static final int VERSION_CODE = 7;
 }
